73ddf338a4f2cee7ed0c0f4878bcd461cded7047
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-17 15:02:59 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if :reject 
+  accepts_nested_attributes_for :user, reject_if: :reject
   
   def reject(attributes)
     attributes['username'].blank?
